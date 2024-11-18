eceb6c06cc5a32c47e42db407533a8bd04eb7d65
Sagar10joshi <152054575+Sagar10joshi@users.noreply.github.com>
2024-11-18 21:24:59 +0530

    Update QuizHomePage.jsx

--- vite-project/src/QuizHomePage.jsx
+++ vite-project/src/QuizHomePage.jsx
@@ -114,7 +114,7 @@ function Quiz({ questions }) {
 
       const token = sessionStorage.getItem('token'); // Fetch the token
       // POST the score to your backend
-      const response = await fetch('http://localhost:5000/score', {
+      const response = await fetch('https://quizfinal-rc7v9q1o4-sagars-projects-0f20619e.vercel.app/score', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
