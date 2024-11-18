156a957e4f3699d73b8df72b338bfc19821b5546
Sagar10joshi <152054575+Sagar10joshi@users.noreply.github.com>
2024-11-18 22:46:50 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,16 +12,16 @@ dotenv.config({
 
 const app = express();
 // CORS Configuration
-const corsOptions = {
-  origin: ["https://quizfinal-iacx-4oa5w1cx4-sagars-projects-0f20619e.vercel.app"], // Add your frontend URL here
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  credentials: true, // Allow cookies and credentials
-};
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests
+// const corsOptions = {
+//   origin: ["https://quizfinal-iacx-4oa5w1cx4-sagars-projects-0f20619e.vercel.app"], // Add your frontend URL here
+//   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+//   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+//   credentials: true, // Allow cookies and credentials
+// };
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions)); // Handle preflight requests
   
-// app.use(cors({}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 
